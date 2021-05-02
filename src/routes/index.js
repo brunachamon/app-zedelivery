bc10579cce2defa62acdeed956c73025ed77be82
Bruna Chamon <brunachammon@gmail.com>
2021-05-02 20:54:07 -0300

    Fixing role in tests

---
+++ src/routes/index.js
@@ -0,0 +1,36 @@
+import { lazy, Suspense } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+import { Paths } from 'utils/constants'
+import { Loader } from '../components/Loader'
+
+const Routes = props => {
+  const Home = lazy(() => import('../pages/Home'))
+  const ProductsList = lazy(() => import('../pages/ProductsList'))
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <Route exact path={Paths.root}>
+        <Redirect to={Paths.home} {...props} />
+      </Route>
+
+      <Route
+        exact
+        path={Paths.home}
+        component={Home}
+        {...props} />
+
+      <Route
+        exact
+        path={`${Paths.products}/${Paths.list}`}
+        component={ProductsList}
+        {...props} />
+
+      {/* <Route
+        render={() => <Redirect to={Paths.login} />}
+      /> */}
+    </Suspense>
+  )
+}
+
+export default Routes
