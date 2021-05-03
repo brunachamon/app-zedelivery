0fa55c299a1f10fbe09b4198bd8aa7fddaddb804
Bruna Chamon <brunachammon@gmail.com>
2021-05-03 18:12:34 -0300

    Cleaning app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 
-import Loader from './components/Loader'
 import Routes from './routes'
 import './App.scss'
 
-const App = () => {
-
-  // console.log('>>', import.meta.env)
-
-  return (
-    <div className="App">
-      <Loader />
-      <p></p>
-
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </div>
+)
 
 export default App
