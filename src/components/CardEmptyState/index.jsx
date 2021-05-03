ecbd9fb7ed855fab3266d9d2c7a46210c49d63fc
Bruna Chamon <brunachammon@gmail.com>
2021-05-03 18:10:18 -0300

    Including Card for Empty state

---
+++ src/components/CardEmptyState/index.jsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+const CardEmptyState = () => {
+  return (
+    <div className="card-empty-state">
+      <p>Nenhum resultado encontrado =(</p>
+    </div>
+  )
+}
+
+export default CardEmptyState
---
+++ src/components/CardEmptyState/test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import CardEmptyState from './'
+
+describe('CardEmptyState', () => {
+  it('should render a component with text to not found content', async () => {
+    render(<CardEmptyState />)
+
+    expect(screen.getByText('Nenhum resultado encontrado =(')).toBeInTheDocument()
+  })
+})
