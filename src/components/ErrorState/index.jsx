19565326ad25c1a2d7f4756a21ea81a5f2742ba7
Bruna Chamon <brunachammon@gmail.com>
2021-05-03 18:10:44 -0300

    Including component to error state

---
+++ src/components/ErrorState/index.jsx
@@ -0,0 +1,9 @@
+const ErrorState = () => {
+  return (
+    <div>
+      Algum erro aconteceu e não conseguimos completar a operação =(
+    </div>
+  )
+}
+
+export default ErrorState
---
+++ src/components/ErrorState/test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import ErrorState from './'
+
+describe('ErrorState', () => {
+  it('should render a component with text to error in request', async () => {
+    render(<ErrorState />)
+
+    expect(screen.getByText('Algum erro aconteceu e não conseguimos completar a operação =(')).toBeInTheDocument()
+  })
+})
