384b08a19cf3e19c4523ebd7660ce78143f6b542
Bruna Chamon <brunachammon@gmail.com>
2021-05-03 18:09:26 -0300

    Setting font Nunito

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import Loader from './components/Loader'
@@ -10,18 +9,9 @@ const App = () => {
   // console.log('>>', import.meta.env)
 
   return (
-    <div>
-      <header className="header">
-        <Loader />
-        <p></p>
-        {/* <Modal>
-          <Modal.Header>oaifhiahfe</Modal.Header>
-          <Modal.Body>dfnsfnsifn</Modal.Body>
-        </Modal> */}
-
-        {/* <a href="#modal" className="btn btn-big">Modal!</a> */}
-
-      </header>
+    <div className="App">
+      <Loader />
+      <p></p>
 
       <BrowserRouter>
         <Routes />
