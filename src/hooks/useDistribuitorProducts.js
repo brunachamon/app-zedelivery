7ac72bb0781028578dfd820ddaab01ad79d9ffe4
Bruna Chamon <brunachammon@gmail.com>
2021-05-10 17:34:00 -0300

    Removing some attributes

--- src/hooks/useDistribuitorProducts.js
+++ src/hooks/useDistribuitorProducts.js
@@ -37,32 +37,14 @@ const useDistribuitorProducts = (location) => {
         products(categoryId: $categoryId, search: $search) {
           id
           title
-          rgb
           images {
             url
           }
           productVariants {
-            availableDate
-            productVariantId
             price
-            inventoryItemId
             shortDescription
             title
-            published
-            volume
-            volumeUnit
-            description
             subtitle
-            components {
-              id
-              productVariantId
-              productVariant {
-                id
-                title
-                description
-                shortDescription
-              }
-            }
           }
         }
       }
